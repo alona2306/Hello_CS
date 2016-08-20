814b7dc1490c15bf85cfef1cf7635aaa0ab238cf
Alona <alona.kalenyk@gmail.com>
2016-08-20 22:41:09 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Url(@"https://ndb.nal.usda.gov/ndb/foods");
+
+            var client = new WebClient();
+            var downloadString = client.DownloadString("https://ndb.nal.usda.gov/ndb/foods");
+
+            Console.WriteLine();
+        }
+    }
+}
