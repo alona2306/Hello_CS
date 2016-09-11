75de4108e281e99be6fc567e50cff320288be637
Gonen Beneish <Gonenb@gmail.com>
2016-09-11 17:08:55 +0300

    Collect nutrient info

--- Program.cs
+++ Program.cs
@@ -11,12 +11,20 @@ namespace Hello_CS
 {
     class Program
     {
-        struct sFood
+        public struct sNutrient
         {
-            string foodName;
-            string foodGroup;
-            string url;
+            public string name;
+            public string units;
+            public float valuePer100gram;
+        }
+
+        public struct sFood
+        {
+            public string foodName;
+            public string foodGroup;
+            public string url;
             // more info
+            public List<sNutrient> nutirentList;
         }
 
         static void Main(string[] args)
@@ -29,11 +37,11 @@ namespace Hello_CS
             var htmlWeb = new HtmlWeb();
 
             // Creates an HtmlDocument object from an URL
-            //var document = htmlWeb.Load("https://ndb.nal.usda.gov/ndb/foods?format=&count=&max=10000&sort=&fgcd=&manu=&lfacet=&qlookup=&offset=0&order=desc");
-            var document = htmlWeb.Load("https://ndb.nal.usda.gov/ndb/foods?format=&count=&max=10&sort=&fgcd=&manu=&lfacet=&qlookup=&offset=0&order=desc");
+            //var foodlist = htmlWeb.Load("https://ndb.nal.usda.gov/ndb/foods?format=&count=&max=10000&sort=&fgcd=&manu=&lfacet=&qlookup=&offset=0&order=desc");
+            var foodlist = htmlWeb.Load("https://ndb.nal.usda.gov/ndb/foods?format=&count=&max=10&sort=&fgcd=&manu=&lfacet=&qlookup=&offset=0&order=desc");
 
             // Targets a specific node
-            var foodTable = document.DocumentNode.SelectNodes("//table")[1];
+            var foodTable = foodlist.DocumentNode.SelectNodes("//table")[1];
 
             char[] charsToTrim = {' ', '\t', '\n'}; 
            
@@ -49,12 +57,41 @@ namespace Hello_CS
                 if (!int.TryParse(columns[0].InnerText.Trim(charsToTrim), out id)) continue;
               
                 var detailsUrl = string.Concat("https://ndb.nal.usda.gov", columns[0].Element("a").GetAttributeValue("href", "unknown"));
-                var description = columns[1].InnerText.Trim(charsToTrim);
+                var foodName = columns[1].InnerText.Trim(charsToTrim);
                 var foodgroup = columns[2].InnerText.Trim(charsToTrim);
 
-                Console.WriteLine(string.Format("ID: {0}\tDescription: {1}\tFood group: {2}\nUrl: {3}\n\n", id, description, foodgroup, detailsUrl));
+                Console.WriteLine(string.Format("ID: {0}\tDescription: {1}\tFood group: {2}\nUrl: {3}\n\n", id, foodName, foodgroup, detailsUrl));
+
+                var foodReport = htmlWeb.Load(detailsUrl);
+                var foodReportTable = foodReport.DocumentNode.SelectNodes("//table")[0].SelectNodes("//tbody")[0];
+
+                sFood foodInfo;
+                foodInfo.foodName = foodName;
+                foodInfo.foodGroup = foodgroup;
+                foodInfo.url = detailsUrl;
+                foodInfo.nutirentList = new List<sNutrient>();
+
+                foreach (var nutrient in foodReportTable.SelectNodes("//tr"))
+                {
+                    var nutrientValue = nutrient.SelectNodes("th|td");
+                    if (nutrientValue.Count == 1) continue;
+
+                    //if (!int.TryParse(nutrientValue.)continue;
+
+                    var nutrientName = nutrientValue[0].InnerText.Trim(charsToTrim);
+                    var units = nutrientValue[1].InnerText.Trim(charsToTrim);
+                    var per100gram = nutrientValue[2].InnerText.Trim(charsToTrim);
+
+                    sNutrient nutrientInfo;
+                    nutrientInfo.name = nutrientName;
+                    nutrientInfo.units = units;
+                    nutrientInfo.valuePer100gram = float.Parse(per100gram);
+                    foodInfo.nutirentList.Add(nutrientInfo);
+
+                }
 
                 // add food to dictionary
+                foodDictionary.Add(foodName, foodInfo);
             }
 
             // save dictionary to file
